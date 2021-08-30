bd141c68831752c41428e334a9b30ff14ec10f7c
psianion <excerpt.snm@gmail.com>
2021-08-30 21:43:33 +0530

    Gym Pages 80%

--- pages/tom/eu/[id].js
+++ pages/tom/eu/[id].js
@@ -14,7 +14,7 @@ export async function getServerSideProps({ query }) {
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
     projectId: process.env.GOOGLE_PROJECTID,
     credentials: {
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
     },
   });
