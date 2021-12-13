5bbf43f3c01288bec2b6f3707445d60dc4078162
psianion <excerpt.snm@gmail.com>
2021-12-13 15:47:03 +0530

    NA E4 Added

---
+++ pages/tom/na/e4.js
@@ -0,0 +1,184 @@
+import styled from "styled-components";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import GymHero from "../../../src/components/ToM/GymHero";
+import Footer from "../../../src/components/Footer";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BackButton from "../../../src/components/BackButton";
+import NAE4 from "../../../src/assets/data/NAE4";
+import PokemonTeam from "../../../src/components/ToM/PokemonTeam";
+
+function EliteFour() {
+  return (
+    <Container>
+      <Head>
+        <title>NA Elite Four | PvP HQ</title>
+      </Head>
+      <GymHero gymName={"Elite Four"} gymLocation={"North America Region"} />
+      <BackButton text={"All NA Gyms"} path={"/tom/na"} />
+      <TextContainer>
+        <h1>
+          The <Highlight>Elite Four </Highlight>defending North America Region
+          will be :
+        </h1>
+      </TextContainer>
+      {NAE4.map((E4) => (
+        <Region key={E4.id}>
+          <h5>
+            {E4.typing1} & {E4.typing2} Elite
+          </h5>
+          <h3>{E4.name}</h3>
+          <p>CLICK TO COPY TRAINER CODE!</p>
+          <LeaderCode
+            onClick={() => {
+              navigator.clipboard.writeText(E4.trainerCode);
+            }}
+          >
+            <div>{E4.trainerCode}</div>
+            <FontAwesomeIcon
+              style={{ marginLeft: "0.5rem", fontSize: "1rem" }}
+              icon={faCopy}
+            />
+          </LeaderCode>
+          <PokemonTeam pokemon={E4.pokemon} />
+        </Region>
+      ))}
+      <Footer />
+    </Container>
+  );
+}
+
+const Container = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LeaderCode = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${({ theme }) => `${theme.primary1}`};
+  padding: 0rem 2rem;
+  margin-bottom: 0.5rem;
+  font-weight: 500;
+  transition: all 0.2s ease-in-out;
+  color: ${({ theme }) => theme.secondary1};
+
+  &:hover {
+    color: ${({ theme }) => theme.highlight0};
+  }
+
+  @media (max-width: 768px) {
+    padding: 0.25rem 2rem;
+    font-weight: 600;
+    font-size: 1.5rem;
+  }
+`;
+
+const Region = styled(motion.div)`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+  font-family: "Poppins", sans-serif;
+  border-bottom: 0.3rem solid ${({ theme }) => theme.primary1};
+  background-color: ${({ theme }) => `${theme.primary1}20`};
+
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+  padding: 0.2rem 0.5rem;
+  margin-top: 1rem;
+
+  h3 {
+    font-size: 2rem;
+    font-family: "Poppins", sans-serif;
+    margin-bottom: 0.3rem;
+    font-weight: 500;
+  }
+
+  h5 {
+    font-size: 1.25rem;
+    letter-spacing: 0.2rem;
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-top: 1rem;
+    color: ${({ theme }) => `${theme.secondary2}80`};
+  }
+
+  p {
+    font-size: 1rem;
+    letter-spacing: 0.1rem;
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    color: ${({ theme }) => theme.primary2};
+    text-transform: uppercase;
+  }
+
+  &:hover {
+    border-bottom: 0.3rem solid ${({ theme }) => theme.highlight0};
+    background-color: ${({ theme }) => `${theme.primary1}40`};
+  }
+
+  @media (max-width: 768px) {
+    width: 20rem;
+    border-radius: 0.1rem;
+    font-size: 1.2rem;
+
+    h3 {
+      margin-bottom: 0rem;
+      font-size: 1.75rem;
+    }
+
+    h5 {
+      letter-spacing: 0.2rem;
+      font-size: 1rem;
+    }
+
+    p {
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+const TextContainer = styled(motion.div)`
+  width: 80rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  text-align: center;
+
+  h1 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+    font-size: 1.5rem;
+    // text-transform: uppercase;
+  }
+
+  @media (max-width: 768px) {
+    width: 90%;
+    flex-direction: column-reverse;
+    align-items: center;
+    height: fit-content;
+
+    h1 {
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+const Highlight = styled(motion.span)`
+  color: ${({ theme }) => theme.highlight0};
+  font-size: 1.6rem;
+
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+export default EliteFour;
---
+++ src/assets/data/NAE4.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    id: 1,
+    name: "HotPoket777",
+    pokemon: "lickitung,diggersby,pidgeot,obstagoon,cresselia,deoxys-defense",
+    typing1: "Normal",
+    typing2: "Psychic",
+    trainerCode: "4430 2989 5421",
+  },
+  {
+    id: 2,
+    name: "Reis2Occasion",
+    pokemon: "nidoqueen,mandibuzz,roserade,qwilfish,scrafty,sableye",
+    typing1: "Dark",
+    typing2: "Poison",
+    trainerCode: "8716 3641 6180",
+  },
+  {
+    id: 3,
+    name: "DragOns1lk",
+    pokemon: "jellicent,zweilous,marowak-alola,gengar,altaria,froslass",
+    typing1: "Ghost",
+    typing2: "Dragon",
+    trainerCode: "8218 4234 7175",
+  },
+  {
+    id: 4,
+    name: "SpeediestChief2",
+    pokemon: "azumarill,scizor,swampert,stunfisk-galar,lapras,skarmory",
+    typing1: "Water",
+    typing2: "Steel",
+    trainerCode: "7592 3310 2972",
+  },
+];
