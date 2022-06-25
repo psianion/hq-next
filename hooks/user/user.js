46ab6d86817978a7b76f93204aa55af4953f15f3
psianion <excerpt.snm@gmail.com>
2022-06-26 02:38:47 +0530

    Login/Profile Working

--- hooks/user/user.js
+++ hooks/user/user.js
@@ -1,9 +1,11 @@
 import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
+const URL = process.env.NEXT_PUBLIC_API_URL;
+
 export const useUser = () => {
   const fetchUser = async () => {
-    const res = await axios.get("http://localhost:3001/profile", {
+    const res = await axios.get(URL + "/profile", {
       withCredentials: true,
       headers: {
         Accept: "application/json",
@@ -28,7 +30,7 @@ export const useSetupProfile = () => {
 
   const setupProfile = async (id, data) => {
     try {
-      const res = await axios.post(`http://localhost:3001/profile/setup`, {
+      const res = await axios.post(URL + "/profile/setup", {
         id: id,
         data: data,
       });
